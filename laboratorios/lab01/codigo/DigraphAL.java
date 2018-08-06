fa7e20461738608a1583cbf8a5233424a9196fc5
juan camilo arenas florez <jarena12@eafit.edu.co>
2018-08-05 22:36:16 -0500

    5/08/2018 10:36

---
+++ laboratorios/lab01/codigo/DigraphAL.java
@@ -0,0 +1,21 @@
+package com.jarena;
+
+import javafx.util.Pair;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class DigraphAL extends Graph {
+    private ArrayList<LinkedList<Pair<Integer, Integer>>> nodo;
+
+    public DigraphAL(int size) {
+        super(size);
+        nodo = new ArrayList<>();
+        for (int i = 0; i < size + 1; i++) {
+            nodo.add(new LinkedList<>());
+        }
+    }
+
+    public void addArc(int source, int destination,int weight) {
+        nodo.get(source).add(new Pair<>(destination, weight));
+    }
+}
